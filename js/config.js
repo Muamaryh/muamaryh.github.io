a66336c841730cd2ca320693766ca2c64b042adb
muamaryh <muamaryh26@gmail.com>
2023-03-27 21:13:46 +0700

    update

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ const config = {
   prayTime: {
     // lihat di https://github.com/benangmerah/wilayah/blob/master/datasources/daftar-nama-daerah.csv
     // default menggunakan kabupaten Cianjur
-    latitude: -6.82008,
-    longtitude: 107.173859,
+    latitude: 6.8122,
+    longtitude: 107.1398,
   },
 };
 
